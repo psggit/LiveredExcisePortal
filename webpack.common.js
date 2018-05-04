fc678ac328e4a8c87d6a7d407b9ba84b4c5ee009
Madhur Garg <madhur@hipbar.com>
2018-05-04 15:08:35 +0530

    vendor caching

--- webpack.common.js
+++ webpack.common.js
@@ -15,7 +15,7 @@ module.exports = {
     })
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].[chunkhash].js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/'
 
