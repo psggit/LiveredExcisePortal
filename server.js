3e68d259827a1a413d9588eb46a2a8ab280cea29
Madhur Garg <madhur@hipbar.com>
2018-05-04 13:27:52 +0530

    wildcard route and history api fallback for index.html

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 
-app.use(express.static(__dirname + '/dist'))
+app.use(express.static(path.join(__dirname, 'dist')))
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'), (err) => {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
 app.listen(8080)
 console.log('Server is running on port 8080')
