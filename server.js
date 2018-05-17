7b2ecdc8c29863a320c15dc5da0e57398decf325
Madhur Garg <madhur@hipbar.com>
2018-05-17 22:35:46 +0530

    not using server side rendering for react

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import path from 'path'
 import React from 'react'
-import { renderToNodeStream } from 'react-dom/server'
-import { ServerStyleSheet } from 'styled-components'
-import App from './src/react-apps/login'
+// import { renderToNodeStream } from 'react-dom/server'
+// import { ServerStyleSheet } from 'styled-components'
+// import App from './src/react-apps/login'
 
 const app = express()
 
@@ -23,15 +23,15 @@ if (env === 'production') {
 app.use(express.static(path.join(__dirname, 'dist')))
 
 app.get('/*', (req, res) => {
-  const sheet = new ServerStyleSheet()
-  const jsx = sheet.collectStyles(<App />)
-  const stream = sheet.interleaveWithNodeStream(renderToNodeStream(jsx))
-  stream.pipe(res).on('end', () => res.end())
-  // res.sendFile(path.join(__dirname, 'dist/index.html'), (err) => {
-  //   if (err) {
-  //     res.status(500).send(err)
-  //   }
-  // })
+  // const sheet = new ServerStyleSheet()
+  // const jsx = sheet.collectStyles(<App />)
+  // const stream = sheet.interleaveWithNodeStream(renderToNodeStream(jsx))
+  // stream.pipe(res).on('end', () => res.end())
+  res.sendFile(path.join(__dirname, 'dist/index.html'), (err) => {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
 })
 app.listen(8080)
 console.log('Server is running on port 8080')
