2f0729927ef52037795cd8f579eb7db5b0797930
Madhur Garg <madhur@hipbar.com>
2018-06-21 14:32:23 +0530

    button component

---
+++ src/components/button/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './button.scss'
+
+class Button extends React.Component {
+  getClassName() {
+    let className = 'btn-default'
+    if (this.props.primary) {
+      className = 'btn-primary'
+    } else if (this.props.secondary) {
+      className = 'btn-secondary'
+    }
+
+    return className
+  }
+
+  render() {
+    return (
+      <button onClick={this.props.onClick} className={`btn ${this.getClassName()}`}>
+        { this.props.children }
+      </button>
+    )
+  }
+}
+
+export default Button
