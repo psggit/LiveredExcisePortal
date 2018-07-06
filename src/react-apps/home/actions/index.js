2c92c2bae0b43a98cdccb751698f6f5a9c7ec11d
Madhur Garg <madhur@hipbar.com>
2018-07-06 12:36:26 +0530

    updated action creator names

--- src/react-apps/home/actions/index.js
+++ src/react-apps/home/actions/index.js
@@ -1,11 +1,26 @@
 import * as ActionTypes from './../constants/actions'
 
-export const fetchLiveOrders = (data) => ({
-  type: ActionTypes.REQUEST_FETCH_LIVE_ORDERS,
+export const fetchInProgressOTTP = data => ({
+  type: ActionTypes.REQUEST_FETCH_IN_PROGRESS_OTTP,
   data
 })
 
-export const fetchHistoryOrders = (data) => ({
-  type: ActionTypes.REQUEST_FETCH_HISTORY_ORDERS,
+export const fetchHistoryOTTP = data => ({
+  type: ActionTypes.REQUEST_FETCH_HISTORY_OTTP,
+  data
+})
+
+export const fetchOTTPDetail = data => ({
+  type: ActionTypes.REQUEST_FETCH_OTTP_DETAIL,
+  data
+})
+
+export const fetchSquadMembers = data => ({
+  type: ActionTypes.REQUEST_FETCH_SQUAD_MEMBERS,
+  data
+})
+
+export const updateSquadMember = data => ({
+  type: ActionTypes.REQUEST_UPDATE_SQUAD_MEMBER,
   data
 })
