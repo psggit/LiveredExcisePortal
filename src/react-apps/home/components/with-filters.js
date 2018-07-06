c274b73544b4f2e0c47585f8697f92ef83455f2b
Madhur Garg <madhur@hipbar.com>
2018-07-06 12:39:40 +0530

    add setFilters method

--- src/react-apps/home/components/with-filters.js
+++ src/react-apps/home/components/with-filters.js
@@ -2,14 +2,48 @@ import React from 'react'
 import Filter from '@components/filter'
 
 class WithFilters extends React.Component {
+  constructor() {
+    // const today = new Date()
+    // today.setUTCHours(0, 0, 0, 0)
+    // const tommorrow = new Date(today.getTime())
+    // tommorrow.setDate(tommorrow.getDate() + 1)
+    // tommorrow.setUTCHours(0, 0, 0, 0)
+
+    super()
+    this.state = {}
+    this.setFilters = this.setFilters.bind(this)
+  }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   if (JSON.stringify(this.state) !== JSON.stringify(nextState)) return true
+  //   return false
+  // }
+
+  setFilters(filters) {
+    this.setState({
+      status: filters.statusFilter,
+      from: filters.dateFilter.from,
+      to: filters.dateFilter.to
+    })
+  }
   render() {
+    const { children } = this.props
     return (
-      <div style={{ marginTop: '62px' }}>
+      <div style={{ marginTop: '62px', padding: '20px' }}>
         {
-          <Filter currentRoute={this.props.currentRoute} />
+          <Filter
+            setFilters={this.setFilters}
+            filters={this.props.filters}
+          />
         }
         <div style={{ marginTop: '20px' }}>
-          { this.props.children }
+          {
+            React.Children.map(children, (child) => {
+              return React.cloneElement(child, {
+                filters: this.state
+              })
+            })
+          }
         </div>
       </div>
     )
