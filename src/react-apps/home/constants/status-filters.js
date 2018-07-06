95c9905ccaabbb0044ce9ae9255a0fbd13a6c879
Madhur Garg <madhur@hipbar.com>
2018-07-06 17:54:22 +0530

    added status filters

---
+++ src/react-apps/home/constants/status-filters.js
@@ -0,0 +1,12 @@
+export const liveFilters = [
+  { value: 'all', label: '-All-' },
+  { value: 'returning', label: 'Returning' },
+  { value: 'ongoing', label: 'On going' }
+]
+
+
+export const historyFilters = [
+  { value: 'all', label: '-All-' },
+  { value: 'cancelled', label: 'Cancelled' },
+  { value: 'delivered', label: 'Delivered' }
+]
