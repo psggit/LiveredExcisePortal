af56f17f2ff86c2e49b426b4a68f2818959e1fb9
Madhur Garg <madhur@hipbar.com>
2018-07-20 22:53:34 +0530

    added company name,  ottp expiry time

--- src/react-apps/home/components/ottp-detail.js
+++ src/react-apps/home/components/ottp-detail.js
@@ -9,10 +9,10 @@ import DeliveryAgentDetail from './deliverer-detail'
 import getIcon from '@components/getIcon'
 import Button from '@components/button/index.js'
 import ErrorBoundary from '@components/error-boundary'
-// import Notes from './Notes'
-// import ShowGmap from './ShowGmap'
+import { mountModal } from '@components/ModalBox/utils'
+import ShowGmap from './show-gmap'
 import moment from 'moment'
-// import { getHasuraId, getIcon } from './../utils'
+import Loader from '@components/loader'
 import '@sass/order-detail.scss'
 
 function getTimeDiff(d2) {
@@ -35,7 +35,6 @@ class OrderDetail extends Component {
     super()
     this.handleClick = this.handleClick.bind(this)
     this.openGmap = this.openGmap.bind(this)
-    this.handleRefersh = this.handleRefersh.bind(this)
     this.unmountOrderDetail = this.unmountOrderDetail.bind(this)
   }
 
@@ -44,15 +43,9 @@ class OrderDetail extends Component {
   }
 
   openGmap() {
-    const { order, retailer, deliverer, customer, plotData, actions, ordersType } = this.props
+    const { ottpId } = this.props.match.params
     mountModal(ShowGmap({
-      id: order.id,
-      ordersType,
-      actions,
-      retailer,
-      deliverer,
-      customer,
-      plotData
+      id: ottpId
     }))
   }
 
@@ -63,85 +56,58 @@ class OrderDetail extends Component {
     })
   }
 
-  componentWillUnmount() {
-    // this.props.actions.setLoading('loadingOrderDetail')
-  }
-
   unmountOrderDetail() {
     this.props.history.goBack()
-    // this.props.history.push('/home/history-orders')
-  }
-
-  handleRefersh() {
-    const { actions, currentOrderId } = this.props
-    actions.setLoading('loadingOrderDetail')
-    actions.fetchOrderDetail(currentOrderId)
+    this.props.actions.setLoadingAll()
   }
 
   render() {
-    const delivererStatus = 'confirmed'
-
-    // const isOrderConfirmed = false
-    // const supportId = parseInt(getHasuraId())
-    // console.log(supportId, order.assignedToId);
-    // const isOrderAssigned = supportId == order.assignedToId
-
     const { actions, OTTPDetailData } = this.props
-
-    // if (order) {
-      // const orderChar = order.status.split('::')[0]
-      // const formula = order.status.split('::')[1]
-      // const article = order.status.split('::')[2]
-      //
-      // const dp_delivered_time = order.deliveredTime
-      // const dp_reached_to_consumer_time = deliverer.reachedToConsumerTime
-      // const cancellation_time = order.cancellationTime
-      // const cancelled_time = order.cancelledTime
-      // const orderPlacedTime = order.orderPlacedTime
-      // const cancellation_return_time = order.cancellationReturnTime
-      // const dp_picked_up_time = order.pickedUpTime
-      // const dp_notified_time = deliverer.notifiedTime
-      // const dp_arrived_at_store_time = deliverer.arrivedAtStoreTime
-      // const dp_accepted_time = deliverer.acceptedTime
-      // const dp_confirmation_time = deliverer.confirmationTime
-    // }
-
-    // if (retailer) {
-      // const retailer_accepted_time = retailer.confirmationTime
-      // const retailer_notified_time = retailer.notifiedTime
-    // }
 
     return (
       <div className="order-detail__wrapper">
         {
-          !this.props.loadingOTTPDetail &&
-          <Fragment>
+          !this.props.loadingOTTPDetail
+          ? <Fragment>
             <div className="order-detail__header">
               <div>
                 <span onClick={this.unmountOrderDetail} style={{ marginRight: '20px', cursor: 'pointer' }}>{ getIcon('back') }</span>
                 <span style={{ fontSize: '18px', color: '#687189' }}>OTTP ID: <b>{`#${this.props.match.params.ottpId}`}</b></span>
               </div>
               <div style={{ marginLeft: '38px', marginTop: '10px' }}>
-                <span
-                  style={{
-                    width: '30px',
-                    height: '30px',
-                    display: 'inline-block',
+                <div style={{ display: 'inline-block', verticalAlign: 'bottom' }}>
+                  <span
+                    style={{
+                      width: '30px',
+                      height: '30px',
+                      display: 'inline-block',
+                      verticalAlign: 'middle',
+                      marginRight: '10px'
+                    }}>
+                    { getIcon(OTTPDetailData.ottp_status) }
+                  </span>
+                  <span style={{
+                    color: '#687189',
+                    fontSize: '18px',
                     verticalAlign: 'middle',
-                    marginRight: '10px'
+                    marginRight: '20px',
+                    textTransform: 'Capitalize'
                   }}>
-                  { getIcon(OTTPDetailData.ottp_status) }
+                  {OTTPDetailData.ottp_status}
                 </span>
-                <span style={{
-                  color: '#687189',
-                  fontSize: '18px',
-                  verticalAlign: 'middle',
-                  marginRight: '20px',
-                  textTransform: 'Capitalize'
-                }}>
-                {OTTPDetailData.ottp_status}
-              </span>
-                <Button primary>Track OTTP</Button>
+                  <Button onClick={this.openGmap} primary>Track OTTP</Button>
+                </div>
+                <div style={{ display: 'inline-block', verticalAlign: 'bottom', marginLeft: '80px' }}>
+                  <div style={{ display: 'inline-block', verticalAlign: 'middle', marginRight: '40px' }}>
+                    <label>OTTP expiry time</label>
+                    <p style={{ color: '#333', fontSize: '13px' }}>{ Moment(OTTPDetailData.ottp_expiry_time).format("DD/MM/YYYY, h:mm") }</p>
+                  </div>
+
+                  <div style={{ display: 'inline-block', verticalAlign: 'middle', marginRight: '40px' }}>
+                    <label>Company name</label>
+                    <p style={{ color: '#333', fontSize: '13px' }}>{ OTTPDetailData.agency }</p>
+                  </div>
+                </div>
               </div>
             </div>
 
@@ -155,12 +121,18 @@ class OrderDetail extends Component {
               <div style={{ width: 'calc(50% - 10px)', marginLeft: '10px' }}>
                 <div>
                   <ErrorBoundary>
-                    <RetailerDetail retailerName={OTTPDetailData.retailer_name} />
+                    <RetailerDetail
+                      retailerAddress={OTTPDetailData.store_address}
+                      retailerName={OTTPDetailData.retailer_name}
+                    />
                   </ErrorBoundary>
                 </div>
                 <div style={{ margin: '20px 0' }}>
                   <ErrorBoundary>
-                    <DeliveryAgentDetail vehicleNumber={OTTPDetailData.vehicle_number} deliveryAgentName={OTTPDetailData.agent_name} />
+                    <DeliveryAgentDetail
+                      vehicleNumber={OTTPDetailData.vehicle_number}
+                      deliveryAgentName={OTTPDetailData.agent_name}
+                    />
                   </ErrorBoundary>
                 </div>
                 <div>
@@ -174,6 +146,7 @@ class OrderDetail extends Component {
               </div>
             </div>
           </Fragment>
+          : <Loader absolute />
         }
       </div>
     )
