1e2cbf8129c1e50bec53926bf98a2cd628f3e081
Madhur Garg <madhur@hipbar.com>
2018-08-03 16:54:36 +0530

    prod config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,23 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 const CompressionPlugin = require('compression-webpack-plugin')
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.s?[ac]ss$/,
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    }
+  },
   plugins: [
     new CompressionPlugin({
       test: /\.js$|\.css$/,
@@ -15,23 +27,27 @@ module.exports = merge(common, {
       threshhold: 10240,
       minRatio: 0.8
     }),
+    new MiniCssExtractPlugin({
+      fileName: '[name].[hash].css',
+      chunkFilename: '[id].[hash].css'
+    })
   ],
   module: {
-    // rules: [
-    //   {
-    //     test: /\.s?[ac]ss$/,
-    //     use: [
-    //       MiniCssExtractPlugin.loader,
-    //       'css-loader',
-    //       'sass-loader'
-    //     ]
-    //   }
-    // ],
     rules: [
       {
         test: /\.s?[ac]ss$/,
-        loader: 'style-loader!css-loader!sass-loader'
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
       }
-    ]
+    ],
+    // rules: [
+    //   {
+    //     test: /\.s?[ac]ss$/,
+    //     loader: 'style-loader!css-loader!sass-loader'
+    //   }
+    // ]
   }
 })
