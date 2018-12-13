665b2f182870e3691594a735c561f0d7767a41ad
Madhur Garg <madhur@hipbar.com>
2018-12-13 13:40:26 +0530

    addded diaog box component

--- src/components/dialog/dialog.story.js
+++ src/components/dialog/dialog.story.js
@@ -4,7 +4,7 @@ import Dialog from './index';
 import OtpInput from './../otp-input'
 import Button from './../button'
 
-storiesOf('Dialog', module).add('Default', () => {
+storiesOf('Dialog', module).add('Without title', () => {
   return (
     <Dialog
       actions={[
@@ -36,6 +36,41 @@ storiesOf('Dialog', module).add('Default', () => {
       <div style={{ marginTop: '30px'  }}>
         <OtpInput />
       </div>
+    </Dialog>
+  )
+})
+
+
+storiesOf('Dialog', module).add('With title', () => {
+  return (
+    <Dialog
+      title="Are you sure to update?"
+      actions={[
+        (
+          <Button
+            onClick={() => {
+              alert('No is clicked')
+            }}
+            secondary
+          >
+          No
+          </Button>
+        ),
+        (
+          <Button
+            onClick={() => {
+              alert('Yes is clicked')
+            }}
+            primary
+          >
+          Yes
+          </Button>
+        )
+      ]}
+    >
+      <p>
+      You are updating rules on the rule engine. This means these changes will be reflected on the system right away after your approval. 
+      </p>
     </Dialog>
   )
 })
--- src/components/dialog/index.js
+++ src/components/dialog/index.js
@@ -2,17 +2,27 @@ import React from 'react'
 import Button from './../button'
 import './dialog.scss'
 import ButtonGroup from './../button-group/index.js'
+import Icon from './../icon'
 
  class Dialog extends React.Component {
   render() {
     return (
      <div className="dialog--container">
-      {/* <div className="dialog--title-bar">
-        <p>
-          { this.props.title }
-        </p>
-      </div> */}
+     <div style={{ position: 'absolute', right: '16px', top: '12px' }}>
+      <Icon name="cross" size="10" />
+     </div>
       <div className="dialog--body">
+        {
+          this.props.title
+          ? (
+            <div className="dialog--title-bar">
+              <p>
+                { this.props.title }
+              </p>
+            </div>
+          )
+          : ''
+        }
         { this.props.children }
       </div>
       <div className="dialog--footer">
