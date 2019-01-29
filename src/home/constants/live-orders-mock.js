73f73afc498e2978483c75f537ea37ed199453ce
Madhur Garg <madhur@hipbar.com>
2019-01-29 13:44:18 +0530

    mock data for live ordrers

---
+++ src/home/constants/live-orders-mock.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    permit_id: 'TS-347783627771',
+    issued_date: '2018.12.01',
+    issued_time: '18:30 PM',
+    dso_name: 'Swiggy',
+    retailer_name: 'Tonique',
+    city: 'Hyderabad',
+    order_amount: 'Rs. 1500',
+    permit_status: 'Active'
+  },
+  {
+    permit_id: 'TS-347783627771',
+    issued_date: '2018.12.01',
+    issued_time: '18:30 PM',
+    dso_name: 'Swiggy',
+    retailer_name: 'Tonique',
+    city: 'Hyderabad',
+    order_amount: 'Rs. 1500',
+    permit_status: 'Active' 
+  },
+  {
+    permit_id: 'TS-347783627771',
+    issued_date: '2018.12.01',
+    issued_time: '18:30 PM',
+    dso_name: 'Swiggy',
+    retailer_name: 'Tonique',
+    city: 'Hyderabad',
+    order_amount: 'Rs. 1500',
+    permit_status: 'Active' 
+  }
+]
